c150f1573aa78304b943ccd727d78ef612f593f0
pblan <p.blaneck@gmail.com>
2021-12-05 21:43:21 +0100

    day06: added template

---
+++ day06/src/main.cpp
@@ -0,0 +1,41 @@
+#include <chrono>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+
+#include <util.h>
+
+static uint32_t task1(std::string filename) {
+
+    auto input = Input<std::string>(filename);
+    
+    return 0;
+}
+
+static uint32_t task2(std::string filename) {
+
+    auto input = Input<std::string>(filename);
+    
+    return 0;
+}
+
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    uint32_t test_answer1{task1("test_input.txt")};
+    uint32_t test_answer2{task2("test_input.txt")};
+
+    uint32_t answer1{task1("input.txt")};
+    uint32_t answer2{task2("input.txt")};
+    
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << "Time: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " μs" << std::endl;
+
+    std::cout << "[Test] Task 1: " << test_answer1 << " " << pass_or_fail(test_answer1, 5) << std::endl;
+    std::cout << "[Test] Task 2: " << test_answer2 << " " << pass_or_fail(test_answer2, 12) << std::endl;
+
+
+    std::cout << "Task 1: " << answer1 << std::endl;
+    std::cout << "Task 2: " << answer2 << std::endl;
+}
